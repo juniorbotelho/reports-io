56e6088f660c70be59c790509218ad21d719bd23
Jamilton Damasceno <jamiltondamasceno@gmail.com>
2021-01-19 17:29:18 -0300

    feat(providers): add user Decorator to discover from firebase class which methods are from user

---
+++ providers/functions/ModelFunction.ts
@@ -0,0 +1,32 @@
+import { getConnection } from "@Providers/Firebase"
+
+export function User() {
+  return (Constructor: ObjectConstructor) => {
+    return class extends Constructor {
+      /**
+       *
+       * Public properties from user likes token,
+       * credentials, info and others...
+       */
+      public subject = {}
+
+      public async signup({ email, password }: IFirebaseUser) {
+        const getUserInfo = await getConnection()
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+
+        // Returns firebase UserCredentials type
+        return getUserInfo
+      }
+
+      public async signin({ email, password }: IFirebaseUser) {
+        const getUserInfo = await getConnection()
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+
+        // Returns firebase UserCredentials type
+        return getUserInfo
+      }
+    }
+  }
+}
---
+++ src/@types/Providers.d.ts
@@ -0,0 +1,4 @@
+declare interface IFirebaseUser {
+  email: string
+  password: string
+}
