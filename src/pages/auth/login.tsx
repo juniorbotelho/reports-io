c9efa47acc8f695b7decec0948d82a702af97a9b
Jamilton Damasceno <jamiltondamasceno@gmail.com>
2021-01-19 11:27:01 -0300

    feat(pages/styles): create login/register pages and your styles

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Login as Container } from "@Style:Pages/Login"
+
+const Login: React.FC = () => (
+  <Container>
+    <div>Login</div>
+  </Container>
+)
+
+export default Login
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Register as Container } from "@Style:Pages/Register"
+
+const Register: React.FC = () => (
+  <Container>
+    <div>Register</div>
+  </Container>
+)
+
+export default Register
