dc4d8778dde56589e70553b96146da3d5e46f421
Jamilton Damasceno <jamiltondamasceno@gmail.com>
2021-01-19 15:36:49 -0300

    feat(providers): add news methods likes getConnection and getRepository to use firebase
    Add new features to use firebase likes active records pattern

--- providers/Firebase.ts
+++ providers/Firebase.ts
@@ -2,6 +2,27 @@
 // must be listed before other Firebase SDKs
 import * as firebase from "firebase/app"
 
+/**
+ * Instance connection from Firebase
+ * class model
+ */
+let connection: Firebase
+
+export function createConnection() {
+  connection = new Firebase()
+}
+
+export function getConnection() {
+  return connection
+}
+
+export function getRepository(Constructor) {
+  class Repository extends Constructor {}
+
+  // Returns an instance from this
+  return new Repository()
+}
+
 export class Firebase {
   /**
    * Firebase configuration
@@ -14,10 +35,19 @@ export class Firebase {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: "G-19VQM5D3DR"
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
   }
 
   private firebase = firebase.default
+
+  /**
+   * Public attributes from this instance,
+   * and make all Firebase attrs
+   */
+  public auth = this.firebase.auth
+  public database = this.firebase.database
+  public firestore = this.firebase.firestore
+  public storage = this.firebase.storage
 
   constructor() {
     this.firebase.initializeApp(this.firebaseConfig)
