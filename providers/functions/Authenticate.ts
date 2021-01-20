3a110dba4738ffcf66936a9f807e97e2f5ade98b
juniorbotelho <botelhosbureau@gmail.com>
2021-01-19 22:12:23 -0300

    refactor(providers): refact name of provider/Model to provider/Authenticate

---
+++ providers/functions/Authenticate.ts
@@ -0,0 +1,43 @@
+import { getConnection } from "@Providers/Firebase"
+
+// Abstract class
+export abstract class BaseAuthenticate implements IFirebaseUserModel {
+  signup(auth: IFirebaseUser): Promise<firebase.default.auth.UserCredential> {
+    throw new Error("Method not implemented.")
+  }
+
+  signin(auth: IFirebaseUser): Promise<firebase.default.auth.UserCredential> {
+    throw new Error("Method not implemented.")
+  }
+}
+
+export function Authenticate() {
+  return <T extends TDecoratorConstructor>(Constructor: T) => {
+    return class extends Constructor {
+      /**
+       *
+       * Public properties from user likes token,
+       * credentials, info and others...
+       */
+      public subject = {}
+
+      public async signup({ email, password }: IFirebaseUser) {
+        const getUserInfo = await getConnection()
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+
+        // Returns firebase UserCredentials type
+        return getUserInfo
+      }
+
+      public async signin({ email, password }: IFirebaseUser) {
+        const getUserInfo = await getConnection()
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+
+        // Returns firebase UserCredentials type
+        return getUserInfo
+      }
+    }
+  }
+}
