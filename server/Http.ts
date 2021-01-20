4477b843e593c895f9873b7e8ab434cd2e8457f8
Jamilton Damasceno <jamiltondamasceno@gmail.com>
2021-01-20 12:21:41 -0300

    feat(server): create express Http server to intergrate with nextjs

---
+++ server/Http.ts
@@ -0,0 +1,45 @@
+import express, { Request, Response } from "express"
+import next from "next"
+
+const environment = process.env.NODE_ENV !== "production"
+const app = next({ dev: environment })
+const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000
+
+// Handle exception server
+class ErrorServerConnection {
+  constructor(error: Error) {
+    throw error
+  }
+}
+
+// Handle method execution
+function Exec() {
+  return (Target: { new (): Server }) => {
+    new Target()
+      .connect()
+      .then((status) => console.info(status))
+      .catch((error) => new ErrorServerConnection(error))
+  }
+}
+
+@Exec()
+export class Server {
+  public async connect() {
+    try {
+      await app.prepare()
+      const server = express()
+
+      // Bind next handler to express
+      server.all("*", (request: Request, response: Response) => {
+        return handle(request, response)
+      })
+
+      server.listen(port, (error?: Error) => {
+        if (error) throw new ErrorServerConnection(error)
+      })
+    } catch (error) {
+      throw new ErrorServerConnection(error)
+    }
+  }
+}
