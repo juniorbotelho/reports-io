0de6d3e2d8b4e0ef7808c7c3d4b67862e7ed58f9
juniorbotelho <botelhosbureau@gmail.com>
2021-01-21 01:43:17 -0300

    feat(pages,@types): create page to register with /auth/register router

---
+++ src/@types/Html.d.ts
@@ -0,0 +1,1 @@
+declare type HTMLInputEvent = React.FormEvent<HTMLInputElement>
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,11 +1,71 @@
 import React from "react"
+import Head from "next/head"
 
 import { Register as Container } from "@Style:Pages/Register"
 
-const Register: React.FC = () => (
-  <Container>
-    <div>Register</div>
-  </Container>
-)
+import { Form } from "@Components/Form"
+import { Input } from "@Components/Input"
+
+import { Title } from "@Style:Components/Title"
+import { Span } from "@Style:Components/Span"
+import { Label } from "@Style:Components/Label"
+import { Button } from "@Style:Components/Button"
+import { Link } from "@Style:Components/Link"
+
+const Register: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Reports.io/Register</title>
+      </Head>
+
+      <Container>
+        <Title className="Title_Register">Register</Title>
+
+        <Form className="Form_Register" action="/users/signup" method="POST">
+          <Label htmlFor="email" className="Label_Register">
+            <Span className="Span_Register">Email:</Span>
+            <Input
+              className="Input_Register"
+              type="text"
+              name="email"
+              placeholder="Email"
+            />
+          </Label>
+
+          <Label htmlFor="username" className="Label_Register">
+            <Span className="Span_Register">Username:</Span>
+            <Input
+              className="Input_Register"
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
+          </Label>
+
+          <Label htmlFor="password" className="Label_Register">
+            <Span className="Span_Register">Password:</Span>
+            <Input
+              className="Input_Register"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </Label>
+
+          <Label className="Label_Register Label_Undo">
+            <Link href="#" className="Link_Register">
+              Forgot password?
+            </Link>
+          </Label>
+
+          <Button type="submit" className="Button_Register">
+            Register
+          </Button>
+        </Form>
+      </Container>
+    </>
+  )
+}
 
 export default Register
