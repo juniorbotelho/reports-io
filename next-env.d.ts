1efe2be97217bba48145993c648debb1025be8fe
juniorbotelho <botelhosbureau@gmail.com>
2021-01-22 19:48:10 -0300

    feat(dependencies,config): create a next type into next-env; Add CryptoJS to dependencie

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next-images" />
+
+declare type ReportsPageContext = import("next").NextPageContext
