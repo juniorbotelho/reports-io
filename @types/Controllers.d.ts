846682c08c7f699f82904892daf4237d924ac774
juniorbotelho <botelhosbureau@gmail.com>
2021-01-25 22:03:10 -0300

    chore: update type from Controllers and ignore eslint on @types folder

--- @types/Controllers.d.ts
+++ @types/Controllers.d.ts
@@ -1,15 +1,9 @@
 declare namespace User {
-  export type Request = import("express").Request<
-    {},
-    {},
-    UserBodyRequest,
-    UserQueryRequest
-  >
+  export type Request = import("express").Request<{}, {}, UserBodyRequest, UserQueryRequest>
   export type Response = import("express").Response
   export type UserModel = import("@App:Models/User").User
 
-  interface UserBodyRequest extends UserModel {}
-
+  interface UserBodyRequest extends UserModel { }
   interface UserQueryRequest {
     token?: string
     validation?: string
