f9cb3402b326844772aa3071796044d34a98bcac
juniorbotelho <junowerther@gmail.com>
2021-01-28 14:53:59 -0300

    refactor: add 'router.push' so that the 'Get early ...' button redirects you to the registry

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,48 +7,55 @@ import { Title } from "@Style:Components/Title"
 import { Div } from "@Style:Components/Div"
 import { Nav } from "@Style:Components/Nav"
 import { Button } from "@Style:Components/Button"
+import { useRouter } from "next/router"
 
-export const Header: React.FC = () => (
-  <Container className="Header">
-    <Div className="Content">
-      <Title className="Title">
-        <>Rep.io</>
-      </Title>
+export const Header: React.FC = () => {
+  const router = useRouter()
 
-      <Nav className="Navbar">
-        <ul className="List">
-          <li className="Link">
-            <Link href="/docs">
-              <a className="Href">Documentation</a>
-            </Link>
-          </li>
-          <li className="Link">
-            <Link href="/pricing">
-              <a className="Href">Pricing</a>
-            </Link>
-          </li>
-          <li className="Link">
-            <Link href="/resources">
-              <a className="Href">Resources</a>
-            </Link>
-          </li>
-          <li className="Link">
-            <Link href="/blog">
-              <a className="Href">Blog</a>
-            </Link>
-          </li>
-        </ul>
-      </Nav>
-    </Div>
+  return (
+    <Container className="Header">
+      <Div className="Content">
+        <Title className="Title">
+          <>Rep.io</>
+        </Title>
 
-    <FiMenu
-      className="Menu"
-      onClick={() => console.log("Menu Hamburguer")}
-      size={32}
-    />
+        <Nav className="Navbar">
+          <ul className="List">
+            <li className="Link">
+              <Link href="/docs">
+                <a className="Href">Documentation</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href="/pricing">
+                <a className="Href">Pricing</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href="/resources">
+                <a className="Href">Resources</a>
+              </Link>
+            </li>
+            <li className="Link">
+              <Link href="/blog">
+                <a className="Href">Blog</a>
+              </Link>
+            </li>
+          </ul>
+        </Nav>
+      </Div>
 
-    <Button className="Button">
-      <span>Get early access</span>
-    </Button>
-  </Container>
-)
+      <FiMenu
+        className="Menu"
+        onClick={() => console.log("Menu Hamburguer")}
+        size={32}
+      />
+
+      <Button
+        className="Button"
+        onClick={() => router.push({ pathname: "/auth/register" })}>
+        <span>Get early access</span>
+      </Button>
+    </Container>
+  )
+}
