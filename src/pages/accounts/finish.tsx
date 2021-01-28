c8c5cda1cfffcb2abbf529ebba3ff53976df19c8
juniorbotelho <junowerther@gmail.com>
2021-01-28 11:30:58 -0300

    refactor: updates the 'finish' page to remove the 'continue' button

--- src/pages/accounts/finish.tsx
+++ src/pages/accounts/finish.tsx
@@ -1,28 +1,18 @@
 import React from "react"
+import Head from "next/head"
 import Lottie from "react-lottie"
 import Validated from "@Asset:Anims/validated.json"
 
-import { Div } from "@Style:Components/Div"
 import { Section } from "@Style:Components/Section"
 import { SubTitle } from "@Style:Components/Title"
-import { Button } from "@Styles/components/Button"
 import { Text } from "@Styles/components/Text"
 
-const Finish: React.FC = () => {
-  // Methods
-  const handleSubmitAuth = () => {
-    fetch("/auth/private")
-      .then(async (response) => {
-        const fetchData = await response.json()
-        console.log(fetchData)
-      })
-      .catch((error) => {
-        // TODO: Sentry Error
-        console.error(error)
-      })
-  }
+const Finish: React.FC = () => (
+  <>
+    <Head>
+      <title>Reports.io - Account Finish</title>
+    </Head>
 
-  return (
     <Section className="Section_Loading Finish">
       <Lottie
         options={{
@@ -33,8 +23,8 @@ const Finish: React.FC = () => {
             preserveAspectRatio: "xMidYMid slice"
           }
         }}
-        width={150}
-        height={150}
+        width={250}
+        height={250}
       />
 
       <SubTitle className="FinishedLoaded">
@@ -46,17 +36,8 @@ const Finish: React.FC = () => {
         to take advantage of all the resources that the platform can provide in
         this initial phase of development,
       </Text>
-
-      <Div className="FinishedDivisor">
-        <Button
-          type="submit"
-          onClick={handleSubmitAuth}
-          className="Button Button_Finish">
-          Continue
-        </Button>
-      </Div>
     </Section>
-  )
-}
+  </>
+)
 
 export default Finish
