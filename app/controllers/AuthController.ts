f40e68a98a1c265e89aba933f8ad4ac5ec9c8425
juniorbotelho <junowerther@gmail.com>
2021-01-28 12:11:29 -0300

    refactor: refactor the 'AuthController' to add important comments

--- app/controllers/AuthController.ts
+++ app/controllers/AuthController.ts
@@ -6,12 +6,12 @@ export class AuthController {
   public static async index({ body }: Request, response: Response) {
     try {
       const repo = getRepository(User)
-
       const user = await repo.findOneOrFail({
         where: { email: body.auth.email }
       })
 
       if (user) {
+        // This condition is called when the user accesses the platform for the first time
         if (user.firsthere) {
           repo.save({ ...user, firsthere: false })
 
@@ -21,6 +21,8 @@ export class AuthController {
           })
         }
 
+        // This other condition is called most of the time, after the user
+        // has already accessed the platform
         if (!user.firsthere) {
           response.status(200).json({
             pathname: "/user/dashboard",
