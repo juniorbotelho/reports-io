23b458b0a5fa44b3da750bb51355ad1c0214b23d
juniorbotelho <junowerther@gmail.com>
2021-01-28 11:42:05 -0300

    feat: create logic to check if it is the user's first time on the platform

--- app/controllers/AuthController.ts
+++ app/controllers/AuthController.ts
@@ -1,5 +1,8 @@
 export class AuthController {
   public static async index(request, response) {
-    response.send("hi")
+    response.status(200).json({
+      pathname: "/user/dashboard",
+      query: {}
+    })
   }
 }
--- app/models/User.ts
+++ app/models/User.ts
@@ -26,6 +26,9 @@ export class User {
   @Column()
   password: string
 
+  @Column({ default: true })
+  firsthere: boolean
+
   @Column({ default: false })
   validated: boolean
 
