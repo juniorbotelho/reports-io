baf6b7af5ae23f393be17c95bbbb2215740a2104
juniorbotelho <junowerther@gmail.com>
2021-01-28 11:33:41 -0300

    refactor: adds a 'Head' to the 'validation' page to identify in the tab

--- src/pages/accounts/validation.tsx
+++ src/pages/accounts/validation.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import Lottie from "react-lottie"
+import Head from "next/head"
 
 import { Section } from "@Style:Components/Section"
 import { Div } from "@Style:Components/Div"
@@ -63,6 +64,10 @@ const Validation: React.FC = () => {
 
   return (
     <>
+      <Head>
+        <title>Reports.io | Account Validation</title>
+      </Head>
+
       {load && (
         <Section className="Section_Loading">
           <Lottie
